ae025ba3d5c26c930ec0039a20051f423cc8ce27
Ben Evans <ben@bensbit.co.uk>
2016-08-03 14:03:45 +0100

    wip

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 const {app, Tray, Menu, BrowserWindow} = require('electron');
 const path = require('path');
-
+const brightness = require('./app')
 const iconPath = path.join(__dirname, 'logo.png');
 let appIcon = null;
 let win = null;
@@ -13,9 +13,16 @@ app.on('ready', function(){
       label: 'Enabled',
       type: 'radio',
       icon: iconPath,
-      checked: true,
-      click: function () {
-        console.log(this)
+      checked: false,
+      click: function (item) {
+        if (item.checked) {
+          item.checked = false
+          console.log('was checked')
+        } else {
+          item.checked = true
+          console.log('now checked')
+        }
+        console.log(arguments)
       }
     },
     {
